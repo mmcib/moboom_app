56a0dadaba9f2eca353942866a426611e57898d2
Damian Perczuk <xmmcibx@gmail.com>
2023-11-14 15:27:32 +0100

    :lipstick: Photos UI - updated

--- lib/features/photos/presentation/components/full_photo_view.dart
+++ lib/features/photos/presentation/components/full_photo_view.dart
@@ -2,7 +2,6 @@ import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:moboom_app/features/photos/data/models/photo_model.dart';
 
-// TODO: Update the UI
 // TODO: Add responsiveness
 // TODO: Add i10n
 class FullPhotoView extends StatelessWidget {
@@ -16,23 +15,46 @@ class FullPhotoView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Color(0xFFF5F5F5),
+      backgroundColor: Colors.black,
       appBar: AppBar(
         centerTitle: true,
         title: Text('Full Photo'),
       ),
-      body: SafeArea(
-        child: Column(
-          children: [
-            Text('title: ${photo.title}'),
-            const SizedBox(height: 12),
-            CachedNetworkImage(
-              imageUrl: photo.url,
-              placeholder: (_, __) => CircularProgressIndicator(color: Colors.deepPurpleAccent),
-              errorWidget: (_, __, ___) => Icon(Icons.error),
+      body: Column(
+        children: [
+          CachedNetworkImage(
+            imageUrl: photo.url,
+            placeholder: (_, __) => CircularProgressIndicator(color: Colors.deepPurpleAccent),
+            errorWidget: (_, __, ___) => Icon(Icons.error),
+          ),
+          Align(
+            child: Container(
+              color: Colors.white,
+              child: Padding(
+                padding: const EdgeInsets.symmetric(
+                  vertical: 6,
+                  horizontal: 8,
+                ),
+                child: Column(
+                  children: [
+                    Text(
+                      'Title',
+                      style: TextStyle(
+                        fontSize: 18,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    Text(
+                      photo.title,
+                      style: TextStyle(fontSize: 16),
+                      textAlign: TextAlign.center,
+                    ),
+                  ],
+                ),
+              ),
             ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
